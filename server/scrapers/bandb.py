bd05c082cdbc06aa7a0648802cf5c54db8469333
Gavi Schneider <gavischneider@gmail.com>
2021-02-02 18:28:47 +0200

    Fixed all the b&b edge cases

--- server/scrapers/bandb.py
+++ server/scrapers/bandb.py
@@ -20,22 +20,84 @@ beers = prods.find_all('a')
 for beer in beers:
     
 
-    #Get beer image
+    # Get beer image
     img = beer.find_all('div', class_='grid__image-ratio')
     if(img):
         links = re.findall(r'(//cdn.shopify.com\S+)', str(img))
         print(links[0])
 
-        # Get url
+        # Get beer url
         l = beer.get('href')
         link = product_base_url + l
         print(link)
     # Get beer data
     data = beer.find('div', class_='grid-product__meta')
     if(data):
+
+        # 1. Beer name
         name = data.find_all('div')[0].text
+        if '-' in name:
+            newtext = name.split('-') #[1]
+            if len(newtext) > 2:
+                # Reverse Hebrew text
+                n = [];
+                for word in newtext:
+                    if word.isascii():
+                        n.append(word)
+                    else:
+                        n.append(word[::-1])
+                newtext = n[-1]
+            else:
+                newtext = name.split('-')[1]
+        else:
+            newtext = name.split(' ') #[1]
+            # Reverse Hebrew text
+            n = [];
+            for word in newtext:
+                if word.isascii():
+                    n.append(word)
+                else:
+                    n.append(word[::-1])
+            newtext = n[::-1]
+            newtext = " ".join(newtext)
+        name = newtext
+        if name[0] == " ":
+            name = name[1:]       
+
+        # 2. Brewery
         brewery = data.find_all('div')[1].text
+        if ' ' in brewery:
+            a = []
+            newbrewery = brewery.split(' ')
+            for word in newbrewery:
+                if word.isascii():
+                    a.append(word)
+                else:
+                    a.append(word[::-1])
+            brewery = a #[::-1]
+
+            all_hebrew = True
+            for w in brewery:
+                if w.isascii():
+                    all_hebrew = False
+            if all_hebrew:
+                brewery = brewery[::-1]
+
+            brewery = " ".join(brewery)
+        else:
+            if not brewery.isascii():
+                brewery = brewery[::-1]
+
+        # 3. Price
         price = data.find_all('div')[2].text
+        if not price[0].isascii():
+            price = price[6:]
+        np = price.split(" ")
+        price = np[::-1]
+        price[0] = price[0][0]
+        price = " ".join(price)
+
         print(name)
         print(brewery)
         print(price)
+        print('\n')
