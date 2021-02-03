092c94ff2f4ec6dd6f8661b39172af7d60a38815
Gavi Schneider <gavischneider@gmail.com>
2021-02-03 13:06:11 +0200

    Added tabs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { TabsBar } from "./components/TabsBar";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -16,18 +16,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TabsBar />
+
         <p>The current time is {currentTime}</p>
       </header>
     </div>
